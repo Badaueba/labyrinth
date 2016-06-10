092d294790e1469bbfebd719fdd5e383f2e93f96
badaueba <alan.develop@outlook.com>
2016-06-09 22:05:27 -0300

    labirinto auto randomizando

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Timer : MonoBehaviour {
 
-	// Use this for initialization
+	public int roomTime = 60;
+	public Text timer;
+	private GameState gameSate;
+	void Awake () {
+		gameSate = Component.FindObjectOfType<GameState> ();
+	} 
 	void Start () {
-	
+		StartCoroutine (UpdateTime ());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	IEnumerator UpdateTime () {
+		while (roomTime > -1) {
+			roomTime--;
+			timer.text = "TIME: " + roomTime;
+			yield return new WaitForSeconds (1);	
+		}
+
+		gameSate.state = GameState.States.GAME_OVER;
+		Application.LoadLevel ("GameOver");
+
 	}
 }
